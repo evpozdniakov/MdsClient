07958206b200214333b10fdb9373faaf33447f05
Evgeniy Pozdnyakov <evp@Evgeniys-MacBook-Pro.local>
2015-03-16 22:02:09 -0400

    Storyboard sketch with 5 views: search, downloads, favourites, recent and most viewed.

--- MdsClient/SearchCatalog.swift
+++ MdsClient/SearchCatalog.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class SearchCatalog: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
